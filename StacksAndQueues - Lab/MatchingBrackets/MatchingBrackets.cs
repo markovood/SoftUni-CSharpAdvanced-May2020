ac34caf7fe2b8cc2708d67033b34251955e1e257
Georgi Markov <markov_ood@abv.bg>
2020-05-18 14:19:05 +0300

    Completed Matching brackets task...

---
+++ StacksAndQueues - Lab/MatchingBrackets/MatchingBrackets.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MatchingBrackets
+{
+    public class MatchingBrackets
+    {
+        public static void Main()
+        {
+            string expression = Console.ReadLine();
+
+            Stack<int> indexes = new Stack<int>();
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '(')
+                {
+                    indexes.Push(i);
+                }
+                else if (expression[i] == ')')
+                {
+                    int startIndex = indexes.Pop();
+                    int endIndex = i;
+                    Console.WriteLine(expression.Substring(startIndex, endIndex - startIndex + 1));
+                }
+            }
+        }
+    }
+}
