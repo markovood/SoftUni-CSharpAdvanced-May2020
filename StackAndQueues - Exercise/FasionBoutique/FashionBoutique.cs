b260b52e206af51704a78c15c21c1b8b65b06c1d
Georgi Markov <markov_ood@abv.bg>
2020-05-18 15:25:11 +0300

    Completed Fashion boutique task...

---
+++ StackAndQueues - Exercise/FasionBoutique/FashionBoutique.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FashionBoutique
+{
+    public class FashionBoutique
+    {
+        public static void Main()
+        {
+            int[] clothes = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rackCapacity = int.Parse(Console.ReadLine());
+
+            Stack<int> box = new Stack<int>(clothes);
+            int sum = 0;
+            int rackCounter = 0;
+            while (box.Count != 0)
+            {
+                sum += box.Peek();
+                if (sum < rackCapacity)
+                {
+                    box.Pop();
+                }
+                else if (sum == rackCapacity && box.Count > 0)
+                {
+                    rackCounter++;
+                    box.Pop();
+                    sum = 0;
+                }
+                else
+                {
+                    rackCounter++;
+                    sum = 0;
+                }
+            }
+
+            if (sum != 0)
+            {
+                rackCounter++;
+            }
+            Console.WriteLine(rackCounter);
+        }
+    }
+}
