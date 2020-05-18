8025caff1bc288b80511caf2b741d9eeb696351b
Georgi Markov <markov_ood@abv.bg>
2020-05-18 14:54:43 +0300

    Completed Hot potato task...

---
+++ StacksAndQueues - Lab/HotPotatoe/HotPotatoe.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HotPotato
+{
+    public class HotPotato
+    {
+        public static void Main()
+        {
+            string[] children = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            int tosses = int.Parse(Console.ReadLine());
+
+            Queue<string> theRing = new Queue<string>(children);
+            while (theRing.Count > 1)
+            {
+                for (int i = 1; i < tosses; i++)
+                {
+                    theRing.Enqueue(theRing.Dequeue());
+                }
+
+                Console.WriteLine($"Removed {theRing.Dequeue()}");
+            }
+
+            Console.WriteLine($"Last is {theRing.Dequeue()}");
+        }
+    }
+}
