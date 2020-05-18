faea7b16a26aee26b869377b11a1583d21af1572
Georgi Markov <markov_ood@abv.bg>
2020-05-18 15:22:39 +0300

    Completed Fast food task...

---
+++ StackAndQueues - Exercise/FastFood/FastFood.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FastFood
+{
+    public class FastFood
+    {
+        public static void Main()
+        {
+            int quantity = int.Parse(Console.ReadLine());
+            int[] orders = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> queue = new Queue<int>(orders);
+            Console.WriteLine(queue.Max());
+            while (queue.Count != 0)
+            {
+                if (quantity >= queue.Peek())
+                {
+                    quantity -= queue.Dequeue();
+                }
+                else
+                {
+                    Console.WriteLine($"Orders left: {string.Join(" ", queue)}");
+                    return;
+                }
+            }
+
+            Console.WriteLine("Orders complete");
+        }
+    }
+}
