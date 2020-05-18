3aaed8209cc8fc0b80cc37aa3218f5f35c77ee90
Georgi Markov <markov_ood@abv.bg>
2020-05-18 16:10:02 +0300

    Completed Cups and bottles...

---
+++ StackAndQueues - Exercise/CupsAndBottles/CupsAndBottles.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CupsAndBottles
+{
+    public class CupsAndBottles
+    {
+        public static void Main()
+        {
+            int[] cupsCapacity = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> cups = new Queue<int>(cupsCapacity);
+
+            int[] bottlesCapacity = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> bottles = new Stack<int>(bottlesCapacity);
+
+            int wasted = 0;
+            while (true)
+            {
+                if (cups.Count <= 0)
+                {
+                    Console.WriteLine($"Bottles: {string.Join(' ', bottles)}");
+                    break;
+                }
+                else if (bottles.Count <= 0)
+                {
+                    Console.WriteLine($"Cups: {string.Join(' ', cups)}");
+                    break;
+                }
+
+                int currentBtl = bottles.Peek();
+                int currentCup = cups.Peek();
+
+                while (true)
+                {
+                    currentCup -= currentBtl;
+                    if (currentCup > 0)
+                    {
+                        bottles.Pop();
+                        currentBtl = bottles.Peek();
+                    }
+                    else
+                    {
+                        cups.Dequeue();
+                        wasted += Math.Abs(currentCup);
+                        bottles.Pop();
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Wasted litters of water: {wasted}");
+        }
+    }
+}
