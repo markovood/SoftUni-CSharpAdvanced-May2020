7c6c69d5d45c6e6df3b1c80df4d6ceb87a130511
Georgi Markov <markov_ood@abv.bg>
2020-05-18 15:14:20 +0300

    Completed Basic queue operations task...

---
+++ StackAndQueues - Exercise/BasicQueueOperations/BasicQueueOperations.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BasicQueueOperations
+{
+    public class BasicQueueOperations
+    {
+        public static void Main()
+        {
+            int[] firstInputLine = Console.ReadLine().
+                Split(' ', StringSplitOptions.RemoveEmptyEntries).
+                Select(int.Parse).
+                ToArray();
+
+            int elementsToDequeue = firstInputLine[1];
+            int elementToFind = firstInputLine[2];
+
+            int[] secondInputLine = Console.ReadLine().
+                Split(' ', StringSplitOptions.RemoveEmptyEntries).
+                Select(int.Parse).
+                ToArray();
+
+            Queue<int> queue = new Queue<int>(secondInputLine);
+            for (int i = 0; i < elementsToDequeue; i++)
+            {
+                queue.Dequeue();
+            }
+
+            if (queue.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else if (queue.Contains(elementToFind))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(queue.Min());
+            }
+        }
+    }
+}
