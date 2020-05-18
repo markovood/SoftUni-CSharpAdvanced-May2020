0025b25408b5e4755d33d08cfc83bddaad90bd2d
Georgi Markov <markov_ood@abv.bg>
2020-05-18 16:50:55 +0300

    Completed Primary diagonal task...

---
+++ MultidimensionalArrays - Lab/PrimaryDiagonal/PrimaryDiagonal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace PrimaryDiagonal
+{
+    public class PrimaryDiagonal
+    {
+        public static void Main()
+        {
+            int matrixSize = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[matrixSize, matrixSize];
+            for (int i = 0; i < matrixSize; i++)
+            {
+                int[] currentRow = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                for (int j = 0; j < currentRow.Length; j++)
+                {
+                    matrix[i, j] = currentRow[j];
+                }
+            }
+
+            int sum = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                sum += matrix[i, i];
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
