655c49ea64e480511a527796eade0796891134a9
Georgi Markov <markov_ood@abv.bg>
2020-05-18 14:51:39 +0300

    Completed Supermarket task...

---
+++ StacksAndQueues - Lab/Supermarket/Supermarket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Supermarket
+{
+    public class Supermarket
+    {
+        public static void Main()
+        {
+            Queue<string> people = new Queue<string>();
+            while (true)
+            {
+                string name = Console.ReadLine();
+                if (name == "End")
+                {
+                    break;
+                }
+
+                if (name == "Paid")
+                {
+                    while (people.Count != 0)
+                    {
+                        Console.WriteLine(people.Dequeue());
+                    }
+                }
+                else
+                {
+                    people.Enqueue(name);
+                }
+            }
+
+            Console.WriteLine($"{people.Count} people remaining.");
+        }
+    }
+}
