60491cb1aa9232bcf7a690f965d71453597b4e50
Georgi Markov <markov_ood@abv.bg>
2020-05-18 15:47:52 +0300

    Completed Truck tour task...

---
+++ StackAndQueues - Exercise/TruckTour/TruckTour.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TruckTour
+{
+    public class TruckTour
+    {
+        public static void Main()
+        {
+            // reading input
+            int numberOfPumps = int.Parse(Console.ReadLine());
+
+            Queue<int[]> listOfPumps = new Queue<int[]>();
+            for (int i = 0; i < numberOfPumps; i++)
+            {
+                int[] currentPumpDetails = Console.ReadLine().
+                    Split(" ", StringSplitOptions.RemoveEmptyEntries).
+                    Select(int.Parse).
+                    ToArray();
+
+                listOfPumps.Enqueue(currentPumpDetails);
+            }
+
+            // actual calculation
+            int pumpIndex = 0;
+            while (true)
+            {
+                int totalAmountOfPetrol = 0;
+                foreach (var currentPump in listOfPumps)
+                {
+                    int amountOfPetrol = currentPump[0];
+                    int distanceToNextPump = currentPump[1];
+                    totalAmountOfPetrol += amountOfPetrol;
+                    totalAmountOfPetrol -= distanceToNextPump;
+
+                    if (totalAmountOfPetrol < 0)
+                    {
+                        listOfPumps.Enqueue(listOfPumps.Dequeue());
+                        pumpIndex++;
+                        break;
+                    }
+                }
+
+                if (totalAmountOfPetrol >= 0)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(pumpIndex);
+        }
+    }
+}
