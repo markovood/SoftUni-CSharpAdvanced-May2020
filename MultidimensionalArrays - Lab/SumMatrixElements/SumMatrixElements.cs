7023c974f79900b8db388f3457a7848fb80efaca
Georgi Markov <markov_ood@abv.bg>
2020-05-18 16:45:02 +0300

    Completed Sum matrix elements task...

---
+++ MultidimensionalArrays - Lab/SumMatrixElements/SumMatrixElements.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace SumMatrixElements
+{
+    public class SumMatrixElements
+    {
+        public static void Main()
+        {
+            int[] dimensions = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            int[,] matrix = new int[dimensions[0], dimensions[1]];
+
+            int sum = 0;
+            for (int rows = 0; rows < dimensions[0]; rows++)
+            {
+                int[] currentRow = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                for (int col = 0; col < currentRow.Length; col++)
+                {
+                    matrix[rows, col] = currentRow[col];
+                    sum += currentRow[col];
+                }
+            }
+
+            Console.WriteLine(dimensions[0]);
+            Console.WriteLine(dimensions[1]);
+            Console.WriteLine(sum);
+        }
+    }
+}
