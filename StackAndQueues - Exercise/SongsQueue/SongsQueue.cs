07d49b0a4df97047b209fce703338f8a9562d23b
Georgi Markov <markov_ood@abv.bg>
2020-05-18 15:46:05 +0300

    Completed Songs queue task...

---
+++ StackAndQueues - Exercise/SongsQueue/SongsQueue.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SongsQueue
+{
+    public class SongsQueue
+    {
+        public static void Main()
+        {
+            string[] songs = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            Queue<string> songsQueue = new Queue<string>(songs);
+            while (songsQueue.Count > 0)
+            {
+                string[] commandArgs = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                switch (commandArgs[0])
+                {
+                    case "Play":
+                        songsQueue.Dequeue();
+                        break;
+                    case "Add":
+                        string song = string.Join(' ', commandArgs.Skip(1));
+                        if (songsQueue.Contains(song))
+                        {
+                            Console.WriteLine($"{song} is already contained!");
+                        }
+                        else
+                        {
+                            songsQueue.Enqueue(song); 
+                        }
+
+                        break;
+                    case "Show":
+                        Console.WriteLine(string.Join(", ", songsQueue));
+                        break;
+                }
+            }
+
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
