bdd632e111b8ab3893c406d5384f2b8970a1b83b
Georgi Markov <markov_ood@abv.bg>
2020-05-18 14:57:13 +0300

    Completed Traffic jam task...

---
+++ StacksAndQueues - Lab/TrafficJam/TrafficJam.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TrafficJam
+{
+    public class TrafficJam
+    {
+        public static void Main()
+        {
+            int numberOfCarsToPass = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+
+            int carsPassed = 0;
+            Queue<string> cars = new Queue<string>();
+            while (command != "end")
+            {
+                switch (command)
+                {
+                    case "green":
+                        for (int i = 0; i < numberOfCarsToPass; i++)
+                        {
+                            if (cars.Count > 0)
+                            {
+                                Console.WriteLine($"{cars.Dequeue()} passed!");
+                                carsPassed++;
+                            }
+                        }
+
+                        break;
+                    default:
+                        cars.Enqueue(command);  // command is the brand of the car
+                        break;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{carsPassed} cars passed the crossroads.");
+        }
+    }
+}
