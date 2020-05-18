00e0d96cd8b99eab36dbd03b0896dd9fdb87d330
Georgi Markov <markov_ood@abv.bg>
2020-05-18 16:48:45 +0300

    Completed Sum matrix columns task...

---
+++ MultidimensionalArrays - Lab/SumMatrixColumns/SumMatrixColumns.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace SumMatrixColumns
+{
+    public class SumMatrixColumns
+    {
+        public static void Main()
+        {
+            int[] dimensions = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            int[,] matrix = new int[dimensions[0], dimensions[1]];
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                int[] currentRow = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                for (int col = 0; col < currentRow.Length; col++)
+                {
+                    matrix[rows, col] = currentRow[col];
+                }
+            }
+
+            int sum = 0;
+            for (int cols = 0; cols < matrix.GetLength(1); cols++)
+            {
+                for (int rows = 0; rows < matrix.GetLength(0); rows++)
+                {
+                    sum += matrix[rows, cols];
+                }
+
+                Console.WriteLine(sum);
+                sum = 0;
+            }
+        }
+    }
+}
