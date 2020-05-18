4274db5b0e772644ff91f8c8e64dd1e65fc72c6a
Georgi Markov <markov_ood@abv.bg>
2020-05-18 14:05:00 +0300

    Completed Reverse strings task...

--- StacksAndQueues - Lab/ReverseStrings/ReverseStrings.cs
+++ StacksAndQueues - Lab/ReverseStrings/ReverseStrings.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace ReverseStrings
 {
@@ -6,7 +8,29 @@ namespace ReverseStrings
     {
         public static void Main()
         {
+            // Console.WriteLine("Please enter a string...");
+            string userInputStr = Console.ReadLine();
 
+            string output = ReverseUserInputString(userInputStr);
+            Console.WriteLine(output);
+        }
+
+        private static string ReverseUserInputString(string userInputStr)
+        {
+            var reverser = new Stack<char>();
+            foreach (var symbol in userInputStr)
+            {
+                reverser.Push(symbol);
+            }
+
+            StringBuilder output = new StringBuilder();
+            int reverserCount = reverser.Count;
+            for (int i = 0; i < reverserCount; i++)
+            {
+                output.Append(reverser.Pop());
+            }
+
+            return output.ToString();
         }
     }
 }
