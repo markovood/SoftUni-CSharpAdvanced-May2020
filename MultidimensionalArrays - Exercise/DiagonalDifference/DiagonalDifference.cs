8d6f701f3ac689edc9edc0ca71f037e8446e1272
Georgi Markov <markov_ood@abv.bg>
2020-05-19 16:59:29 +0300

    Completed Diagonal difference task...

--- MultidimensionalArrays - Exercise/DiagonalDifference/DiagonalDifference.cs
+++ MultidimensionalArrays - Exercise/DiagonalDifference/DiagonalDifference.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace DiagonalDifference
 {
@@ -6,7 +7,35 @@ namespace DiagonalDifference
     {
         public static void Main()
         {
+            int N = int.Parse(Console.ReadLine());
 
+            int[,] matrix = new int[N, N];
+
+            for (int i = 0; i < N; i++)
+            {
+                int[] rowValues = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int j = 0; j < N; j++)
+                {
+                    matrix[i, j] = rowValues[j];
+                }
+            }
+
+            int primaryDiagonalSum = 0;
+            for (int i = 0; i < N; i++)
+            {
+                primaryDiagonalSum += matrix[i, i];
+            }
+
+            int secondaryDiagonalSum = 0;
+            for (int i = 0, j = matrix.GetLength(1) - 1; i < matrix.GetLength(0); i++, j--)
+            {
+                secondaryDiagonalSum += matrix[i, j];
+            }
+
+            Console.WriteLine(Math.Abs(primaryDiagonalSum - secondaryDiagonalSum));
         }
     }
 }
