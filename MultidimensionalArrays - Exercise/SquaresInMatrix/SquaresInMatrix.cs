7adc63eb4241bf46829053500156ca045ff7da8a
Georgi Markov <markov_ood@abv.bg>
2020-05-19 17:21:04 +0300

    Completed Squares in matrix task...

---
+++ MultidimensionalArrays - Exercise/SquaresInMatrix/SquaresInMatrix.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace SquaresInMatrix
+{
+    public class SquaresInMatrix
+    {
+        public static void Main()
+        {
+            int[] matrixSizes = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = matrixSizes[0];
+            int cols = matrixSizes[1];
+
+            char[,] matrix = new char[rows, cols];
+
+            // filling-up the matrix
+            for (int row = 0; row < rows; row++)
+            {
+                char[] rowValues = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = rowValues[col];
+                }
+            }
+
+            // look for squares
+            int squares = 0;
+            for (int i = 0; i < matrix.GetLength(0) - 1; i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+                {
+                    if (matrix[i,j] == matrix[i,j+1] &&
+                        matrix[i,j] == matrix[i+1,j] &&
+                        matrix[i,j] == matrix[i+1,j+1])
+                    {
+                        squares++;
+                    }
+                }
+            }
+
+            Console.WriteLine(squares);
+        }
+    }
+}
