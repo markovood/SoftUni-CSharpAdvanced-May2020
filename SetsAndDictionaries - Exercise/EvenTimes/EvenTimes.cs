10a45e701b3bb1945d4bd4edb9ce0a16dc796d94
Georgi Markov <markov_ood@abv.bg>
2020-05-21 21:29:45 +0300

    Completed Even times task...

---
+++ SetsAndDictionaries - Exercise/EvenTimes/EvenTimes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EvenTimes
+{
+    public class EvenTimes
+    {
+        public static void Main()
+        {
+            int N = int.Parse(Console.ReadLine());
+            Dictionary<int, int> numbersAndCount = new Dictionary<int, int>();
+            for (int i = 0; i < N; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (numbersAndCount.ContainsKey(number))
+                {
+                    numbersAndCount[number]++;
+                }
+                else
+                {
+                    numbersAndCount.Add(number, 1);
+                }
+            }
+
+            Console.WriteLine(numbersAndCount
+                                .Where(n => n.Value % 2 == 0)
+                                .First()
+                                .Key);
+        }
+    }
+}
