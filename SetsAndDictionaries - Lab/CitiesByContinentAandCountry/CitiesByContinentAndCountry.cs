b984e93a514106254dfa21d90fb5a9340b642b41
Georgi Markov <markov_ood@abv.bg>
2020-05-21 20:13:42 +0300

    Completed Cities by continent and country task...

---
+++ SetsAndDictionaries - Lab/CitiesByContinentAandCountry/CitiesByContinentAndCountry.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CitiesByContinentAndCountry
+{
+    public class CitiesByContinentAndCountry
+    {
+        public static void Main()
+        {
+            int linesCount = int.Parse(Console.ReadLine());
+
+            var citiesByContinentAndCountry = new Dictionary<string, Dictionary<string, List<string>>>();
+            for (int i = 0; i < linesCount; i++)
+            {
+                string[] info = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string continent = info[0];
+                string country = info[1];
+                string city = info[2];
+
+                if (citiesByContinentAndCountry.ContainsKey(continent))
+                {
+                    if (citiesByContinentAndCountry[continent].ContainsKey(country))
+                    {
+                        citiesByContinentAndCountry[continent][country].Add(city);
+                    }
+                    else
+                    {
+                        citiesByContinentAndCountry[continent].Add(country, new List<string>() { city });
+                    }
+                }
+                else
+                {
+                    citiesByContinentAndCountry.Add(continent, new Dictionary<string, List<string>>()
+                    {
+                        { country, new  List<string>() { city} }
+                    });
+                }
+            }
+
+            // print
+            foreach (var continent in citiesByContinentAndCountry)
+            {
+                Console.WriteLine($"{continent.Key}:");
+                foreach (var country in continent.Value)
+                {
+                    Console.WriteLine($"{country.Key} -> {string.Join(", ", country.Value)}");
+                }
+            }
+        }
+    }
+}
