980c0bf9a263c56579113d649dd278939bf5351c
Georgi Markov <markov_ood@abv.bg>
2020-05-21 20:07:37 +0300

    Initial commit and completed Count same values in array task...

---
+++ SetsAndDictionaries - Lab/CountSameValuesInArray/CountSameValuesInArray.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountSameValuesInArray
+{
+    public class CountSameValuesInArray
+    {
+        public static void Main()
+        {
+            double[] inputArr = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            Dictionary<double, int> occurences = new Dictionary<double, int>();
+
+            // count all elements and write them to a dictionary as key == number and value == occurrences
+            foreach (var number in inputArr)
+            {
+                if (occurences.ContainsKey(number))
+                {
+                    occurences[number]++;
+                }
+                else
+                {
+                    occurences[number] = 1;
+                }
+            }
+
+            foreach (var occurence in occurences)
+            {
+                Console.WriteLine($"{occurence.Key} - {occurence.Value} times");
+            }
+        }
+    }
+}
