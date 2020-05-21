1bf7fe376f9351e50c1ca9e16b412c94fc832615
Georgi Markov <markov_ood@abv.bg>
2020-05-21 21:15:16 +0300

    Completed Periodic table task...

---
+++ SetsAndDictionaries - Exercise/PeriodicTable/PeriodicTable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PeriodicTable
+{
+    public class PeriodicTable
+    {
+        public static void Main()
+        {
+            int N = int.Parse(Console.ReadLine());
+            HashSet<string> elements = new HashSet<string>();
+            for (int i = 0; i < N; i++)
+            {
+                string[] line = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                foreach (var item in line)
+                {
+                    elements.Add(item);
+                }
+            }
+
+            var ordered = elements.OrderBy(e => e);
+
+            Console.WriteLine(string.Join(' ', ordered));
+
+        }
+    }
+}
