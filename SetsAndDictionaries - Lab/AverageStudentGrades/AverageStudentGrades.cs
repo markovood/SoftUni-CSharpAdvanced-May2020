726430476afb968580306d3d4aa9a19c15225d7f
Georgi Markov <markov_ood@abv.bg>
2020-05-21 20:09:09 +0300

    Completed Average student grades task...

---
+++ SetsAndDictionaries - Lab/AverageStudentGrades/AverageStudentGrades.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AverageStudentGrades
+{
+    public class AverageStudentGrades
+    {
+        public static void Main()
+        {
+            int numberOfRecords = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<double>> studentsRecords = new Dictionary<string, List<double>>();
+
+            for (int i = 0; i < numberOfRecords; i++)
+            {
+                string[] inputTokens = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string studentName = inputTokens[0];
+                double studentGrade = double.Parse(inputTokens[1]);
+
+                if (studentsRecords.ContainsKey(studentName))
+                {
+                    studentsRecords[studentName].Add(studentGrade);
+                }
+                else
+                {
+                    studentsRecords.Add(studentName, new List<double>() { studentGrade });
+                }
+            }
+
+            foreach (var record in studentsRecords)
+            {
+                string studentName = record.Key;
+                string studentGrades = string.Join(' ', record.Value.Select(r => r.ToString($"{0:f2}")));
+                double average = record.Value.Average();
+
+                Console.WriteLine($"{studentName} -> {studentGrades} (avg: {average:F2})");
+            }
+        }
+    }
+}
