19b15c8467b89ae45cebd3f017e8acef033eabce
Georgi Markov <markov_ood@abv.bg>
2020-05-21 22:29:48 +0300

    Completed Count symbols and Wardrobe tasks...

---
+++ SetsAndDictionaries - Exercise/CountSymbols/CountSymbols.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountSymbols
+{
+    public class CountSymbols
+    {
+        public static void Main()
+        {
+            string text = Console.ReadLine();
+            Dictionary<char, int> symbolsAndCount = new Dictionary<char, int>();
+
+            foreach (var symbol in text)
+            {
+                if (symbolsAndCount.ContainsKey(symbol))
+                {
+                    symbolsAndCount[symbol]++;
+                }
+                else
+                {
+                    symbolsAndCount.Add(symbol, 1);
+                }
+            }
+
+            var ordered = symbolsAndCount.OrderBy(s => s.Key);
+            foreach (var item in ordered)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+        }
+    }
+}
---
+++ SetsAndDictionaries - Exercise/Wardrobe/Wardrobe.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Wardrobe
+{
+    public class Wardrobe
+    {
+        public static void Main()
+        {
+            int N = int.Parse(Console.ReadLine());
+            var colorsItemsAndCount = new Dictionary<string, Dictionary<string, int>>();
+            for (int i = 0; i < N; i++)
+            {
+                //"{color} -> {item1},{item2},{item3}"
+                string[] clothesArgs = Console.ReadLine().Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+                string color = clothesArgs[0];
+                string[] items = clothesArgs[1].Split(',', StringSplitOptions.RemoveEmptyEntries);
+
+                if (colorsItemsAndCount.ContainsKey(color))
+                {
+                    foreach (var item in items)
+                    {
+                        if (colorsItemsAndCount[color].ContainsKey(item))
+                        {
+                            colorsItemsAndCount[color][item]++;
+                        }
+                        else
+                        {
+                            colorsItemsAndCount[color].Add(item, 1);
+                        }
+                    }
+                }
+                else
+                {
+                    var itemsAndCount = new Dictionary<string, int>();
+                    foreach (var item in items)
+                    {
+                        if (itemsAndCount.ContainsKey(item))
+                        {
+                            itemsAndCount[item]++;
+                        }
+                        else
+                        {
+                            itemsAndCount.Add(item, 1);
+                        }
+                    }
+
+                    colorsItemsAndCount.Add(color, itemsAndCount);
+                }
+            }
+
+            string[] colorAndItemToFind = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            foreach (var color in colorsItemsAndCount.Keys)
+            {
+                Console.WriteLine($"{color} clothes:");
+                foreach (var itemAndCount in colorsItemsAndCount[color])
+                {
+                    if (color == colorAndItemToFind[0] && itemAndCount.Key == colorAndItemToFind[1])
+                    {
+                        Console.WriteLine($"* {itemAndCount.Key} - {itemAndCount.Value} (found!)");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"* {itemAndCount.Key} - {itemAndCount.Value}");
+                    }
+                }
+            }
+        }
+    }
+}
