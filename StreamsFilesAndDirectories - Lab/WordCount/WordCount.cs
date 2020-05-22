80b6dc3b4fad603d8e957f6cc0ea6b52ecb11fda
Georgi Markov <markov_ood@abv.bg>
2020-05-22 19:42:40 +0300

    Completed Word count task...

---
+++ StreamsFilesAndDirectories - Lab/WordCount/WordCount.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace WordCount
+{
+    public class WordCount
+    {
+        public static void Main()
+        {
+            char dirSeparator = Path.DirectorySeparatorChar;
+            string inputFileName = "words.txt";
+            string targetFileName = "text.txt";
+            string outputFileName = "result.txt";
+
+            string pathToWords = $"..{dirSeparator}..{dirSeparator}..{dirSeparator}..{dirSeparator}Resources{dirSeparator}03. Word Count{dirSeparator}{inputFileName}";
+            string pathIn = $"..{dirSeparator}..{dirSeparator}..{dirSeparator}..{dirSeparator}Resources{dirSeparator}03. Word Count{dirSeparator}{targetFileName}";
+            string pathOut = $"..{dirSeparator}..{dirSeparator}..{dirSeparator}..{dirSeparator}Resources{dirSeparator}03. Word Count{dirSeparator}{outputFileName}";
+
+            var words = GetWords(pathToWords);
+            var wordsAndCount = CountWords(pathIn, words);
+            WriteDownResult(pathOut, wordsAndCount);
+        }
+
+        private static void WriteDownResult(string pathOut, Dictionary<string, int> wordsAndCount)
+        {
+            var ordered = wordsAndCount.OrderByDescending(x => x.Value);
+            using (var writer = new StreamWriter(pathOut))
+            {
+                foreach (var word in ordered)
+                {
+                    writer.WriteLine($"{word.Key} - {word.Value}");
+                }
+            }
+        }
+
+        private static Dictionary<string, int> CountWords(string pathIn, List<string> words)
+        {
+            var wordsAndCount = new Dictionary<string, int>();
+            using (var textReader = new StreamReader(pathIn))
+            {
+                string line = textReader.ReadLine();
+                while (line != null)
+                {
+                    foreach (var word in words)
+                    {
+                        int wordCount = Count(word, line);
+
+                        if (wordsAndCount.ContainsKey(word))
+                        {
+                            wordsAndCount[word] += wordCount;
+                        }
+                        else
+                        {
+                            wordsAndCount.Add(word, wordCount);
+                        }
+                    }
+
+                    line = textReader.ReadLine();
+                }
+            }
+
+            return wordsAndCount;
+        }
+
+        private static int Count(string word, string line)
+        {
+            int count = 0;
+            var punctuation = line.Where(Char.IsPunctuation).Distinct().ToArray();
+            var words = line.Split().Select(x => x.Trim(punctuation));
+            foreach (var item in words)
+            {
+                if (item.ToLower() == word.ToLower())
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        private static List<string> GetWords(string pathToWords)
+        {
+            List<string> wordsList = new List<string>();
+            using (var wordReader = new StreamReader(pathToWords))
+            {
+                string[] words = wordReader
+                                    .ReadLine()
+                                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                wordsList.AddRange(words);
+            }
+
+            return wordsList;
+        }
+    }
+}
