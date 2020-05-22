63608ffd7f39a194f2116ab753e1649feca0e518
Georgi Markov <markov_ood@abv.bg>
2020-05-22 19:39:46 +0300

    Completed line numbers task and added Resources folder...

---
+++ StreamsFilesAndDirectories - Lab/LineNumbers/LineNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        public static void Main()
+        {
+            char dirSeparator = Path.DirectorySeparatorChar;
+            string inputFileName = "input.txt";
+            string outputFileName = "output.txt";
+            string pathIn = $"..{dirSeparator}..{dirSeparator}..{dirSeparator}..{dirSeparator}Resources{dirSeparator}02. Line Numbers{dirSeparator}{inputFileName}";
+            string pathOut = $"..{dirSeparator}..{dirSeparator}..{dirSeparator}..{dirSeparator}Resources{dirSeparator}02. Line Numbers{dirSeparator}{outputFileName}";
+
+            using (var reader = new StreamReader(pathIn))
+            {
+                using (var writer = new StreamWriter(pathOut))
+                {
+                    int lineNumber = 1;
+                    string line = reader.ReadLine();
+
+                    while (line != null)
+                    {
+                        writer.WriteLine($"{lineNumber}: " + line);
+                        lineNumber++;
+
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
