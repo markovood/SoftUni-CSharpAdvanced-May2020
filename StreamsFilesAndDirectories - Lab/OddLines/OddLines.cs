955e8e465a4fe48ae85225d76b093b38967f95d4
Georgi Markov <markov_ood@abv.bg>
2020-05-22 19:30:43 +0300

    initial commit and completed odd lines task...

---
+++ StreamsFilesAndDirectories - Lab/OddLines/OddLines.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace OddLines
+{
+    public class OddLines
+    {
+        public static void Main()
+        {
+            string pathIn = $"..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}Resources{Path.DirectorySeparatorChar}01. Odd Lines{Path.DirectorySeparatorChar}input.txt";
+            string pathOut = $"..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}Resources{Path.DirectorySeparatorChar}01. Odd Lines{Path.DirectorySeparatorChar}output.txt";
+
+            using (var reader = new StreamReader(pathIn))
+            {
+                using (var writer = new StreamWriter(pathOut))
+                {
+                    int lineCounter = 0;
+                    string line = reader.ReadLine();
+                    while (line != null)
+                    {
+                        if (lineCounter % 2 != 0)
+                        {
+                            writer.WriteLine(line);
+                        }
+
+                        lineCounter++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
