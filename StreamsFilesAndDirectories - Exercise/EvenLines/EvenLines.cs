a9530fe04e253ba1c0f954fa6de606865d62f37a
Georgi Markov <markov_ood@abv.bg>
2020-05-22 22:45:56 +0300

    Completed Even lines task...

--- StreamsFilesAndDirectories - Exercise/EvenLines/EvenLines.cs
+++ StreamsFilesAndDirectories - Exercise/EvenLines/EvenLines.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace EvenLines
 {
@@ -6,7 +9,61 @@ namespace EvenLines
     {
         public static void Main()
         {
+            char separator = Path.DirectorySeparatorChar;
+            string file = "text.txt";
+            string resourcePath = @$"..{separator}..{separator}..{separator}..{separator}Resources";
 
+            string pathIn = Path.Combine(resourcePath, file);
+            using (var reader = new StreamReader(pathIn))
+            {
+                int lineCounter = 0;
+                while (true)
+                {
+                    string line = reader.ReadLine();
+                    if (line == null)
+                    {
+                        break;
+                    }
+
+                    if (lineCounter % 2 == 0)
+                    {
+                        // Replace
+                        line = Replace(line, new char[] { '-', ',', '.', '!', '?' }, '@');
+
+                        // Reverse
+                        line = Reverse(line);
+
+                        Console.WriteLine(line);
+                    }
+
+                    lineCounter++;
+                }
+            }
+        }
+
+        private static string Replace(string line, char[] forbidenSymbols, char newSymbol)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (forbidenSymbols.Contains(line[i]))
+                {
+                    result.Append('@');
+                }
+                else
+                {
+                    result.Append(line[i]);
+                }
+            }
+
+            return result.ToString();
+        }
+
+        private static string Reverse(string line)
+        {
+            string[] words = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            
+            return string.Join(' ', words.Reverse());
         }
     }
 }
