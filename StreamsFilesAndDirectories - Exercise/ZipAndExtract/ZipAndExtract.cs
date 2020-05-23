0fe2c309367d0f9b3be86adb2dab7cbdbaa1ee2f
Georgi Markov <markov_ood@abv.bg>
2020-05-23 21:17:47 +0300

    Completed Zip and extract task...

---
+++ StreamsFilesAndDirectories - Exercise/ZipAndExtract/ZipAndExtract.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace ZipAndExtract
+{
+    public class ZipAndExtract
+    {
+        public static void Main()
+        {
+            char separator = Path.DirectorySeparatorChar;
+            string directoryToZip = $"..{separator}..{separator}..{separator}..{separator}Resources{separator}TestFolder";
+            string archivePath = $"..{separator}..{separator}..{separator}..{separator}Resources{separator}testArchive.zip";
+
+            ZipFile.CreateFromDirectory(directoryToZip, archivePath);
+
+            string directoryToUnzip = $"..{separator}..{separator}..{separator}..{separator}Resources{separator}testArchive-unzipped";
+            ZipFile.ExtractToDirectory(archivePath, directoryToUnzip);
+        }
+    }
+}
