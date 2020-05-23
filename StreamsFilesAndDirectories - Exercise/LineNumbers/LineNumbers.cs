5006db53c01502486d6bca737ec1a9692c0f3ba9
Georgi Markov <markov_ood@abv.bg>
2020-05-23 11:24:06 +0300

    Completed Line numbers task...

---
+++ StreamsFilesAndDirectories - Exercise/LineNumbers/LineNumbers.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        public static void Main()
+        {
+            char separator = Path.DirectorySeparatorChar;
+            string inputFile = "text.txt";
+            string outputFile = "lineNumbersOutput.txt";
+            string resourcePath = @$"..{separator}..{separator}..{separator}..{separator}Resources";
+
+            string pathIn = Path.Combine(resourcePath, inputFile);
+            string pathOut = Path.Combine(resourcePath, outputFile);
+
+            List<string> resultLines = new List<string>();
+            using (var reader = File.OpenText(pathIn))
+            {
+                int lineNumber = 1;
+                while (true)
+                {
+                    string line = reader.ReadLine();
+                    if (line == null)
+                    {
+                        break;
+                    }
+
+                    resultLines.Add(CountLettersAndPunctuations(lineNumber, line));
+                    lineNumber++;
+                }
+            }
+
+            File.AppendAllLines(pathOut, resultLines);
+        }
+
+        private static string CountLettersAndPunctuations(int lineNumber, string line)
+        {
+            StringBuilder newline = new StringBuilder();
+            newline.Append($"Line {lineNumber}: ");
+            int letters = 0;
+            int punctuationMarks = 0;
+            foreach (var symbol in line)
+            {
+                if (char.IsLetter(symbol))
+                {
+                    letters++;
+                }
+                else if (char.IsPunctuation(symbol))
+                {
+                    punctuationMarks++;
+                }
+            }
+
+            newline.Append(line);
+            newline.Append($" ({letters})({punctuationMarks})");
+
+            return newline.ToString();
+        }
+    }
+}
