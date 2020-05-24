4fca661fdb1353807c7883d5f30409466ccea216
Georgi Markov <markov_ood@abv.bg>
2020-05-24 13:43:32 +0300

    Completed Custom min function task...

---
+++ FunctionalProgramming - Exercise/CustomMinFunction/CustomMinFunction.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomMinFunction
+{
+    public class CustomMinFunction
+    {
+        public static void Main()
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            //Func<int[], int> getMinFunc = (x) => 
+            //{
+            //    int min = int.MaxValue;
+            //    foreach (var num in x)
+            //    {
+            //        if (num < min)
+            //        {
+            //            min = num;
+            //        }
+            //    }
+
+            //    return min;
+            //};
+
+            Func<int[], int> getMinFunc = GetMin;
+
+            Console.WriteLine(getMinFunc(numbers));
+        }
+
+        private static int GetMin(int[] collection)
+        {
+            int min = int.MaxValue;
+            foreach (var num in collection)
+            {
+                if (num < min)
+                {
+                    min = num;
+                }
+            }
+
+            return min;
+        }
+    }
+}
