f40aef958cc4235353055cdc98ebc7e1945409ff
Georgi Markov <markov_ood@abv.bg>
2020-05-24 13:29:50 +0300

    Completed Knights of honor task...

---
+++ FunctionalProgramming - Exercise/KnightsOfHonor/KnightsOfHonor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KnightsOfHonor
+{
+    public class KnightsOfHonor
+    {
+        public static void Main()
+        {
+            string[] collection = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            // assign the delegate all the methods that have to be executed
+            Action<string[]> action;
+            action = AppendSir;
+            action += PrintOnConsole;
+
+            // execute the delegate
+            action(collection);
+        }
+
+        private static void AppendSir(string[] collection)
+        {
+            for (int i = 0; i < collection.Length; i++)
+            {
+                collection[i] = collection[i].Insert(0, "Sir ");
+            }
+        }
+
+        private static void PrintOnConsole(string[] collection)
+        {
+            foreach (var item in collection)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
