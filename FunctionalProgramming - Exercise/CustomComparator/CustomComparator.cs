b5ec5ed9b283b445fc01c06eb0bb6819032a07ea
Georgi Markov <markov_ood@abv.bg>
2020-05-24 19:41:33 +0300

    Completed Custom comparator task...

---
+++ FunctionalProgramming - Exercise/CustomComparator/CustomComparator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomComparator
+{
+    public class CustomComparator
+    {
+        public static void Main()
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Comparison<int> comparer = (x, y) =>
+                                            (x % 2 == 0 && y % 2 != 0) ? -1 : 
+                                            (x % 2 != 0 && y % 2 == 0) ? 1 :
+                                            x.CompareTo(y);
+
+            Array.Sort(arr, comparer);
+            Console.WriteLine(string.Join(' ', arr));
+        }
+    }
+}
