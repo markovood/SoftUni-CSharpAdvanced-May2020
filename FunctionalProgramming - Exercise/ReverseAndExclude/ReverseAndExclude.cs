0378d2ade2fefcf25e2c8fa68a474b75cf8ca42e
Georgi Markov <markov_ood@abv.bg>
2020-05-24 16:14:51 +0300

    Completed reverse and exclude task...

---
+++ FunctionalProgramming - Exercise/ReverseAndExclude/ReverseAndExclude.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReverseAndExclude
+{
+    public class ReverseAndExclude
+    {
+        public static void Main()
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            int divisor = int.Parse(Console.ReadLine());
+
+            numbers.Reverse();
+            numbers.RemoveAll(n => n % divisor == 0);
+
+            Console.WriteLine(string.Join(' ', numbers));
+        }
+    }
+}
