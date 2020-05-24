21751b712801b73ddc5c83b6a6158b59b312a36e
Georgi Markov <markov_ood@abv.bg>
2020-05-24 12:37:30 +0300

    initial commit and completed Action print task...

---
+++ FunctionalProgramming - Exercise/ActionPrint/ActionPrint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ActionPrint
+{
+    public class ActionPrint
+    {
+        public static void Main()
+        {
+            string[] names = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Action<IEnumerable<string>> print = PrintOnConsole;
+            
+            print(names);
+        }
+
+        private static void PrintOnConsole(IEnumerable<string> collection)
+        {
+            Console.WriteLine(string.Join(Environment.NewLine, collection));
+        }
+    }
+}
