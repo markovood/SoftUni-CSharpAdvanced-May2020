d3a5cf87fd249f7c0441b76897d33e7898edd2cc
Georgi Markov <markov_ood@abv.bg>
2020-05-26 13:04:25 +0300

    Completed List of predicates task...

---
+++ FunctionalProgramming - Exercise/ListOfPredicates/ListOfPredicates.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListOfPredicates
+{
+    public class ListOfPredicates
+    {
+        public static void Main()
+        {
+            int end = int.Parse(Console.ReadLine());
+            IEnumerable<int> dividers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse);
+
+            int[] range = Enumerable.Range(1, end).ToArray();
+
+            List<Predicate<int>> predicates = new List<Predicate<int>>();
+            foreach (var divider in dividers)
+            {
+                predicates.Add(x => x % divider == 0);
+            }
+
+            // print all numbers from the range which are divisible to all divisors together
+            for (int i = 0; i < range.Length; i++)
+            {
+                bool isDivisable = true;
+
+                foreach (var predicate in predicates)
+                {
+                    if (!predicate(range[i]))
+                    {
+                        isDivisable = false;
+                        break;
+                    }
+                }
+
+                if (isDivisable)
+                {
+                    Console.Write("{0} ", range[i]);
+                }
+            }
+        }
+    }
+}
