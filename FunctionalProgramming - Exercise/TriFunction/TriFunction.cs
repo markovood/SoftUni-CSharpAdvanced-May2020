dfdd9fdb794e322bd8dbe491728f986062beb345
Georgi Markov <markov_ood@abv.bg>
2020-05-27 14:00:56 +0300

    Completed TriFunction task..

---
+++ FunctionalProgramming - Exercise/TriFunction/TriFunction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace TriFunction
+{
+    public class TriFunction
+    {
+        public static void Main()
+        {
+            int sum = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Func<string, int, bool> isSumGreaterOrEqualFunc = (name, sum) => name.Sum(x => x) >= sum;
+
+            Func<string[], Func<string, int, bool>, string> getFirstName = (arr, func) =>
+            {
+                foreach (var name in arr)
+                {
+                    if (func(name, sum))
+                    {
+                        return name;
+                    }
+                }
+
+                return null;
+            };
+
+            Console.WriteLine(getFirstName(names, isSumGreaterOrEqualFunc));
+        }
+    }
+}
