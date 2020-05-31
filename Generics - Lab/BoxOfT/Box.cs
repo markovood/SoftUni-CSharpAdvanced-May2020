3975d2c9ab3e7854779f24fe7c69aaca8d11db26
Georgi Markov <markov_ood@abv.bg>
2020-05-31 21:26:54 +0300

    Completed generics lab...

---
+++ Generics - Lab/BoxOfT/Box.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private T[] innerArray;
+        private int capacity;
+        private int size;
+
+        public Box()
+        {
+            this.size = 0;
+            this.capacity = 4;
+            this.innerArray = new T[this.capacity];
+        }
+
+        public int Count { get { return this.size; } }
+
+        public void Add(T element)
+        {
+            if (this.size == this.capacity)
+            {
+                Grow();
+            }
+
+            this.innerArray[this.size] = element;
+            this.size++;
+        }
+
+        public T Remove()
+        {
+            if (this.size == 0)
+            {
+                throw new ArgumentException("The box is empty!");
+            }
+            else
+            {
+                this.size--;
+                return this.innerArray[this.size];
+            }
+        }
+
+        private void Grow()
+        {
+            this.capacity *= 2;
+            T[] newArray = new T[this.capacity];
+            this.innerArray.CopyTo(newArray, 0);
+            this.innerArray = newArray;
+        }
+    }
+}
---
+++ Generics - Lab/BoxOfT/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BoxOfT
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Box<double> boxInt = new Box<double>();
+            boxInt.Add(1.1);
+            boxInt.Add(1.2);
+            boxInt.Add(1.3);
+            boxInt.Add(1.4);
+            boxInt.Add(1.5);
+
+            Box<int> box = new Box<int>();
+            box.Add(1);
+            box.Add(2);
+            box.Add(3);
+            Console.WriteLine(box.Remove());
+            box.Add(4);
+            box.Add(5);
+            Console.WriteLine(box.Remove());
+        }
+    }
+}
