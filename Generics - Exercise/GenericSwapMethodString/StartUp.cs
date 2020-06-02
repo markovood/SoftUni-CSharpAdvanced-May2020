fff7b7b147bbc116f299b4fdd84e4c0306408cc8
Georgi Markov <markov_ood@abv.bg>
2020-06-02 16:29:27 +0300

    Completed generic swap method strings task...

---
+++ Generics - Exercise/GenericSwapMethodString/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericSwapMethodString
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int N = int.Parse(Console.ReadLine());
+            List<Box<string>> boxes = new List<Box<string>>();
+            for (int i = 0; i < N; i++)
+            {
+                string str = Console.ReadLine();
+                Box<string> box = new Box<string>(str);
+                boxes.Add(box);
+            }
+
+            int[] indexes = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Swap(boxes, indexes[0], indexes[1]);
+            Console.WriteLine(string.Join(Environment.NewLine, boxes));
+        }
+
+        public static void Swap<T>(List<T> list, int index1, int index2)
+        {
+            T temp = list[index1];
+            list[index1] = list[index2];
+            list[index2] = temp;
+        }
+    }
+}
