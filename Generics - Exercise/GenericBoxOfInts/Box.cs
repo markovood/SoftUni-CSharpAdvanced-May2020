5e65365a9d38f422bfbc36f55621f6e5b6933326
Georgi Markov <markov_ood@abv.bg>
2020-06-02 16:05:43 +0300

    Completed Generic box of ints task...

---
+++ Generics - Exercise/GenericBoxOfInts/Box.cs
@@ -0,0 +1,17 @@
+﻿namespace GenericBoxOfInts
+{
+    public class Box<T>
+    {
+        private T item;
+
+        public Box(T item)
+        {
+            this.item = item;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.item.GetType().FullName}: {this.item}";
+        }
+    }
+}
---
+++ Generics - Exercise/GenericBoxOfInts/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GenericBoxOfInts
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int N = int.Parse(Console.ReadLine());
+            for (int i = 0; i < N; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                Box<int> box = new Box<int>(num);
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
