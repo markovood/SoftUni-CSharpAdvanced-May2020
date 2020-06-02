6a2277982515429d8614c429d7f47392bd8f76ff
Georgi Markov <markov_ood@abv.bg>
2020-06-02 20:45:51 +0300

    Completed Generic count method strings task...

---
+++ Generics - Exercise/GenericCountMethodString/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GenericCountMethodString
+{
+    public class Box<T>
+    {
+        private T item;
+
+        public Box(T item)
+        {
+            this.item = item;
+        }
+
+        public T Value => this.item;
+
+        public override string ToString()
+        {
+            return $"{this.item.GetType().FullName}: {this.Value}";
+        }
+    }
+}
---
+++ Generics - Exercise/GenericCountMethodString/GenericCountMethodString.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCountMethodString
+{
+    public class GenericCountMethodString
+    {
+        public static void Main()
+        {
+            int N = int.Parse(Console.ReadLine());
+            List<Box<string>> boxes = new List<Box<string>>();
+            for (int i = 0; i < N; i++)
+            {
+                string str = Console.ReadLine();
+                Box<string> box = new Box<string>(str);
+                boxes.Add(box);
+            }
+
+            string elementToCompare = Console.ReadLine();
+            Console.WriteLine(Count(boxes, elementToCompare));
+        }
+
+        public static int Count<T>(List<Box<T>> list, T element) where T : IComparable<T>
+        {
+            int counter = 0;
+            foreach (var item in list)
+            {
+                if (item.Value.CompareTo(element) > 0)
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+    }
+}
