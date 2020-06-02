a1564b651fa3ed3c3ed7a53c8ae7c29106e6e4af
Georgi Markov <markov_ood@abv.bg>
2020-06-02 20:53:12 +0300

    Completed Generic count method double task...

---
+++ Generics - Exercise/GenericCountMethodDouble/Box.cs
@@ -0,0 +1,19 @@
+﻿namespace GenericCountMethodDouble
+{
+    public class Box<T>
+    {
+        private T item;
+
+        public Box(T item)
+        {
+            this.item = item;
+        }
+
+        public T Value => this.item;
+
+        public override string ToString()
+        {
+            return $"{this.item.GetType().FullName}: {this.Value}";
+        }
+    }
+}
---
+++ Generics - Exercise/GenericCountMethodDouble/GenericCountMethodDouble.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCountMethodDouble
+{
+    public class GenericCountMethodDouble
+    {
+        public static void Main()
+        {
+            int N = int.Parse(Console.ReadLine());
+            List<Box<double>> boxes = new List<Box<double>>();
+            for (int i = 0; i < N; i++)
+            {
+                double num = double.Parse(Console.ReadLine());
+                Box<double> box = new Box<double>(num);
+                boxes.Add(box);
+            }
+
+            double numToCompare = double.Parse(Console.ReadLine());
+            Console.WriteLine(Count(boxes, numToCompare));
+        }
+
+        public static int Count<T>(List<Box<T>> list, T element) where T : IComparable<T>
+        {
+            int counter = 0;
+            foreach (var item in list)
+            {
+                if (item.Value.CompareTo(element) > 0)
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+    }
+}
