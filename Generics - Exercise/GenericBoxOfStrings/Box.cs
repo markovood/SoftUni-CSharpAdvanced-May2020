e153cfcd58a6a479e86ed5604029194834360070
Georgi Markov <markov_ood@abv.bg>
2020-06-02 15:40:43 +0300

    initial commit + completed generic box of strings task...

---
+++ Generics - Exercise/GenericBoxOfStrings/Box.cs
@@ -0,0 +1,17 @@
+﻿namespace GenericBoxOfStrings
+{
+    public class Box<T>
+    {
+        private T item;
+
+        public Box(T item)
+        {
+            this.item = item;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.item.GetType().FullName}: {this.item}";
+        }
+    }
+}
---
+++ Generics - Exercise/GenericBoxOfStrings/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GenericBoxOfStrings
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int N = int.Parse(Console.ReadLine());
+            for (int i = 0; i < N; i++)
+            {
+                string str = Console.ReadLine();
+                Box<string> box = new Box<string>(str);
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
