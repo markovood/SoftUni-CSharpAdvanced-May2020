820dace3e9488b7b0577f75f60a355c411eb867d
Georgi Markov <markov_ood@abv.bg>
2020-06-03 14:20:30 +0300

    Small refactoring - added override of ToString() in Tuple class. Renamed Tuple to MyTuple for separating from the built-in class Tuple...

--- Generics - Exercise/Tuple/MyTuple.cs
+++ Generics - Exercise/Tuple/MyTuple.cs
@@ -1,11 +1,11 @@
 ﻿namespace Tuple
 {
-    public class Tuple<T1, T2>
+    public class MyTuple<T1, T2>
     {
         private T1 item1;
         private T2 item2;
 
-        public Tuple(T1 item1, T2 item2)
+        public MyTuple(T1 item1, T2 item2)
         {
             this.Item1 = item1;
             this.Item2 = item2;
@@ -14,5 +14,10 @@
         public T1 Item1 { get => this.item1; private set => this.item1 = value; }
         
         public T2 Item2 { get => this.item2; private set => this.item2 = value; }
+
+        public override string ToString()
+        {
+            return $"{this.Item1} -> {this.Item2}";
+        }
     }
 }
--- Generics - Exercise/Tuple/StartUp.cs
+++ Generics - Exercise/Tuple/StartUp.cs
@@ -9,18 +9,18 @@ namespace Tuple
         {
             // {first name} {last name} {address}
             string[] line1Args = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
-            var tuple = new Tuple<string, string>($"{line1Args[0]} {line1Args[1]}", line1Args[2]);
-            Console.WriteLine($"{tuple.Item1} -> {tuple.Item2}");
+            var tuple = new MyTuple<string, string>($"{line1Args[0]} {line1Args[1]}", line1Args[2]);
+            Console.WriteLine(tuple);
 
             // {name} {liters of beer}
             string[] line2Args = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
-            var otherTuple = new Tuple<string, int>(string.Join(' ', line2Args.SkipLast(1)), int.Parse(line2Args.Last()));
-            Console.WriteLine($"{otherTuple.Item1} -> {otherTuple.Item2}");
+            var otherTuple = new MyTuple<string, int>(string.Join(' ', line2Args.SkipLast(1)), int.Parse(line2Args.Last()));
+            Console.WriteLine(otherTuple);
 
             // {integer} {double}
             string[] line3Args = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
-            var anotherTuple = new Tuple<int, double>(int.Parse(line3Args[0]), double.Parse(line3Args[1]));
-            Console.WriteLine($"{anotherTuple.Item1} -> {anotherTuple.Item2}");
+            var anotherTuple = new MyTuple<int, double>(int.Parse(line3Args[0]), double.Parse(line3Args[1]));
+            Console.WriteLine(anotherTuple);
         }
     }
 }
