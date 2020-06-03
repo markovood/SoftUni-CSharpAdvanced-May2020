1735e3d091095bd210bd952c5d8edd4f2a598255
Georgi Markov <markov_ood@abv.bg>
2020-06-03 21:32:16 +0300

    Completed Stack task...

---
+++ IteratorsAndComparators - Exercise/Stack/Stack.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stack
+{
+    public class Stack<T> : IEnumerable<T>
+    {
+        private const int INITIAL_CAPACITY = 2;
+
+        private T[] innerArr;
+        private int count;
+        private int capacity;
+
+        public Stack()
+        {
+            this.capacity = INITIAL_CAPACITY;
+            this.innerArr = new T[this.capacity];
+        }
+
+
+        public int Count => this.count;
+
+        public void Push(T element)
+        {
+            EnsureCapacity();
+            this.innerArr[this.count] = element;
+            this.count++;
+        }
+
+        public T Pop()
+        {
+            if (this.count == 0)
+            {
+                throw new InvalidOperationException("Stack is empty!");
+            }
+
+            T poped = this.innerArr[this.count - 1];
+            this.innerArr[this.count - 1] = default;
+            this.count--;
+            OptimizeCapacity();
+
+            return poped;
+        }
+
+        private void EnsureCapacity()
+        {
+            if (this.count == this.capacity)
+            {
+                Resize();
+            }
+        }
+
+        private void OptimizeCapacity()
+        {
+            if (this.count < this.capacity / 2)
+            {
+                Shrink();
+            }
+        }
+
+        private void Resize()
+        {
+            this.capacity *= 2;
+            T[] newArr = new T[this.capacity];
+            this.innerArr.CopyTo(newArr, 0);
+
+            this.innerArr = newArr;
+        }
+
+        private void Shrink()
+        {
+            this.capacity /= 2;
+            T[] newArr = new T[this.capacity];
+            this.innerArr.Take(this.count).ToArray().CopyTo(newArr, 0);
+
+            this.innerArr = newArr;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = this.count - 1; i >= 0; i--)
+            {
+                yield return this.innerArr[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ IteratorsAndComparators - Exercise/Stack/StartUp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace Stack
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Stack.Stack<int> stack = new Stack.Stack<int>();
+            while (true)
+            {
+                string cmd = Console.ReadLine();
+                if (cmd == "END")
+                {
+                    break;
+                }
+
+                string[] cmdArgs = cmd.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+                switch (cmdArgs[0])
+                {
+                    case "Push":
+                        foreach (var numStr in cmdArgs.Skip(1))
+                        {
+                            stack.Push(int.Parse(numStr));
+                        }
+
+                        break;
+                    case "Pop":
+                        try
+                        {
+                            stack.Pop();
+                        }
+                        catch (InvalidOperationException)
+                        {
+                            Console.WriteLine("No elements");
+                        }
+
+                        break;
+                }
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                foreach (var num in stack)
+                {
+                    Console.WriteLine(num);
+                } 
+            }
+        }
+    }
+}
