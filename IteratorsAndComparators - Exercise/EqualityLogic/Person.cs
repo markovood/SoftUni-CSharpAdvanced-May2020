3e672324e2e6ce408fecd59239fd0a6120616973
Georgi Markov <markov_ood@abv.bg>
2020-06-05 16:14:59 +0300

    Completed Equality logic task...

---
+++ IteratorsAndComparators - Exercise/EqualityLogic/Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace EqualityLogic
+{
+    // see details on generating overrides of Equals and GetHashCode:
+    // https://docs.microsoft.com/en-us/visualstudio/ide/reference/generate-equals-gethashcode-methods?view=vs-2019
+    public class Person : IComparable<Person>
+    {
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+
+        public int CompareTo(Person person)
+        {
+            int equality = this.Name.CompareTo(person.Name);
+            if (equality == 0)
+            {
+                return this.Age.CompareTo(person.Age);
+            }
+            else
+            {
+                return equality;
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Person)
+            {
+                var objAsPerson = obj as Person;
+                return this.Name == objAsPerson.Name && this.Age == objAsPerson.Age;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(this.Name, this.Age);
+        }
+    }
+}
---
+++ IteratorsAndComparators - Exercise/EqualityLogic/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EqualityLogic
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            SortedSet<Person> sortedPeople = new SortedSet<Person>();
+            HashSet<Person> people = new HashSet<Person>();
+            for (int i = 0; i < N; i++)
+            {
+                string[] personInfo = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string name = personInfo[0];
+                int age = int.Parse(personInfo[1]);
+                var person = new Person(name, age);
+
+                sortedPeople.Add(person);
+                people.Add(person);
+            }
+
+            Console.WriteLine(sortedPeople.Count);
+            Console.WriteLine(people.Count);
+        }
+    }
+}
