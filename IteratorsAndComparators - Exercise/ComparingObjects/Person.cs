f59e4b63f9e61458e16449e823d89ef8ccc85280
Georgi Markov <markov_ood@abv.bg>
2020-06-05 14:52:29 +0300

    Completed comparing objects...

---
+++ IteratorsAndComparators - Exercise/ComparingObjects/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ComparingObjects
+{
+    public class Person : IComparable<Person>
+    {
+        public Person(string name, int age, string town)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Town = town;
+        }
+
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public string Town { get; private set; }
+
+        public int CompareTo(Person person)
+        {
+            int equality = this.Name.CompareTo(person.Name);
+            if (equality == 0)
+            {
+                equality = this.Age.CompareTo(person.Age);
+                if (equality == 0)
+                {
+                    return this.Town.CompareTo(person.Town);
+                }
+                else
+                {
+                    return equality;
+                }
+            }
+            else
+            {
+                return equality;
+            }
+        }
+    }
+}
---
+++ IteratorsAndComparators - Exercise/ComparingObjects/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ComparingObjects
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            List<Person> people = new List<Person>();
+            while (true)
+            {
+                string[] personInfo = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                if (personInfo[0] == "END")
+                {
+                    break;
+                }
+
+                var person = new Person(personInfo[0], int.Parse(personInfo[1]), personInfo[2]);
+                people.Add(person);
+            }
+
+            int N = int.Parse(Console.ReadLine());
+            Person personForComparison = people[N - 1];
+            people.RemoveAt(N - 1);
+            int matches = 1;
+            foreach (var person in people)
+            {
+                if (person.CompareTo(personForComparison) == 0)
+                {
+                    matches++;
+                }
+            }
+
+            string output = matches == 1 ?  "No matches" :  $"{matches} {people.Count + 1 - matches} {people.Count + 1}";
+            Console.WriteLine(output);
+        }
+    }
+}
