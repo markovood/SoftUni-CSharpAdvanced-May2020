69e41429871b9d3dfeb18b71e40261af1b2ee01c
Georgi Markov <markov_ood@abv.bg>
2020-06-06 14:30:30 +0300

    Completed Recursive factorial task...

---
+++ BasicAlgorithms/RecursiveFactorial/RecursiveFactorial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace RecursiveFactorial
+{
+    public class RecursiveFactorial
+    {
+        public static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+            long factorial = Factorial(number);
+
+            Console.WriteLine(factorial);
+        }
+
+        private static long Factorial(int number)
+        {
+            if (number < 0)
+            {
+                throw new InvalidOperationException("Invalid input");
+            }
+
+            if (number == 0)
+            {
+                return 1;
+            }
+
+            int currentNum = number;
+            long currentFactorial = Factorial(number - 1);
+            long finalResult = currentNum * currentFactorial;
+
+            return finalResult;
+        }
+    }
+}
