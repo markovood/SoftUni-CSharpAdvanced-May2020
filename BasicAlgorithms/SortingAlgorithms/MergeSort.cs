29775f5b007c8b623c4205a9a046541411145be8
Georgi Markov <markov_ood@abv.bg>
2020-06-07 14:43:20 +0300

    Completed merge sort task...

---
+++ BasicAlgorithms/SortingAlgorithms/MergeSort.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace SortingAlgorithms
+{
+    public static class MergeSort<T> where T : IComparable<T>
+    {
+        private static T[] aux;
+
+        public static void Sort(T[] arr)
+        {
+            aux = new T[arr.Length];
+            Sort(arr, 0, arr.Length - 1);
+        }
+
+        private static void Sort(T[] arr, int lo, int hi)
+        {
+            // only one element -> already sorted
+            if (lo >= hi)
+            {
+                return;
+            }
+
+            int mid = (lo + hi) / 2;
+
+            // split it into two 'sub-arrays', sort them recursively and then merge them on the way up 
+            Sort(arr, lo, mid);
+            Sort(arr, mid + 1, hi);
+            Merge(arr, lo, mid, hi);
+        }
+
+        private static void Merge(T[] arr, int lo, int mid, int hi)
+        {
+            if (IsLess(arr[mid], arr[mid + 1]))
+            {
+                // if the largest element in the left is smaller than the smallest in the right, the two
+                // subarrays are already merged
+                return;
+            }
+
+            // transfer all elements to the auxiliary array
+            for (int index = lo; index < hi + 1; index++)
+            {
+                aux[index] = arr[index];
+            }
+
+            // merge them back in the main array
+            int i = lo;
+            int j = mid + 1;
+            for (int k = lo; k <= hi; k++)
+            {
+                if (i > mid)
+                {
+                    arr[k] = aux[j++];
+                }
+                else if (j > hi)
+                {
+                    arr[k] = aux[i++];
+                }
+                else if (IsLess(aux[i], aux[j]))
+                {
+                    arr[k] = aux[i++];
+                }
+                else
+                {
+                    arr[k] = aux[j++];
+                }
+            }
+        }
+
+        private static bool IsLess(T value1, T value2)
+        {
+            return value1.CompareTo(value2) < 0;
+        }
+    }
+}
---
+++ BasicAlgorithms/SortingAlgorithms/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace SortingAlgorithms
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int[] unsorted = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            MergeSort<int>.Sort(unsorted);
+            Console.WriteLine(string.Join(' ', unsorted));
+        }
+    }
+}
