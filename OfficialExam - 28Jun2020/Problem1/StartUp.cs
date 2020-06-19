6fd52c49fb64b3870600e011d589dffd82557ed3
Georgi Markov <markov_ood@abv.bg>
2020-06-19 14:04:57 +0300

    Completed skeleton for the exam...

---
+++ OfficialExam - 28Jun2020/Problem1/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Problem1
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
---
+++ OfficialExam - 28Jun2020/Problem3/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Problem3
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
